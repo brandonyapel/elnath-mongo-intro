0bfb71545e5bb7df73514af2e33c466b25df15fe
Brandon Yapel <brandonyapel@gmail.com>
2017-12-07 14:35:51 -0600

    move mongoose connection to separate module

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 
+var bodyParser = require('body-parser');
+
+var mongooseConnection = require('./modules/mongoose-connection');
 var game = require('./routes/game');
+
+var app = express();
 
 var port = process.env.PORT || 5000;
 
@@ -11,16 +13,10 @@ app.use(express.static('server/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var databaseUrl = 'mongodb://localhost:27017/game_chest';
-mongoose.connect(databaseUrl);
 
-mongoose.connection.on('connected', function(){
-    console.log('mongoose connected to :', databaseUrl);
-});
+mongooseConnection.connect();
 
-mongoose.connection.on('error', function(err){
-    console.log('mongoose connection error', error);
-});
+
 
 app.use('/game', game);
 
